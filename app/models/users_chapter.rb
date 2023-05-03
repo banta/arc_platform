5cfc053727f67aea2b96e75f8e1098a2e23e0015
Kihara-Kamotho <kiharaalphan@gmail.com>
2023-05-03 21:03:11 +0300

    Add find by chapter method to users chapter
    Add method that finds chapter leaders

--- app/models/users_chapter.rb
+++ app/models/users_chapter.rb
@@ -2,4 +2,9 @@ class UsersChapter < ApplicationRecord
   # Associations
   belongs_to :chapter
   belongs_to :user
+
+  def self.find_by_chapter(chapter_id)
+    users_chapter = find_by(chapter_id: chapter_id, leader: true)
+    users_chapter&.user&.name || 'No Leader yet'
+  end
 end
